0235ecba3c877363a2a232d71f05b10c02060e73
Nishant Sharma <165388083+Nishantsharma77@users.noreply.github.com>
2025-08-28 12:54:11 +0000

    basics of array

---
+++ Array/NegativeValues.java
@@ -0,0 +1,9 @@
+package Array;
+
+import java.util.Scanner;
+public class NegativeValues {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
