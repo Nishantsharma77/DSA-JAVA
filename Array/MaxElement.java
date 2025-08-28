cfa89495d3bad82eb5e93d29b62ad386dc462b5d
Nishant Sharma <165388083+Nishantsharma77@users.noreply.github.com>
2025-08-28 13:38:03 +0000

    max and min

---
+++ Array/MaxElement.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class MaxElement {
+    public static void main(String args[]) {
+        int[] arr = {-6, 7, 4, 2, 9, 12, 17, 4};
+        // int max = arr[0];
+        //or
+        // int max=Integer.MIN_VALUE;
+        int min =Integer.MAX_VALUE;
+
+        // for (int i = 0; i < arr.length; i++) {
+        //     if (arr[i] > max)  max = arr[i];
+        // }
+
+        // for min
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min)  min = arr[i];
+         }
+
+       System.out.println(max);
+    }
+}
+
+
