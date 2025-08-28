000a145f3fdbb15d8bfa92fdf7483edc7b9d2ca2
Nishant Sharma <165388083+Nishantsharma77@users.noreply.github.com>
2025-08-28 13:18:16 +0000

    Sum of element

--- Array/SumofElements.java
+++ Array/SumofElements.java
@@ -2,6 +2,15 @@ package Array;
 
 public class SumofElements {
     public static void main(String args[]){
-        
+        int[]arr ={-6,8,14,17,65,-18,30};
+        int sum = 0;
+        int product =1;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            product*=arr[i];
+
+        }
+        System.out.println(sum);
+        System.out.println(product);
     }
 }
