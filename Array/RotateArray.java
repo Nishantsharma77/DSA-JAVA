0c53d566a7e52b273c2fb02f57b2e495d9c500cf
Nishant Sharma <165388083+Nishantsharma77@users.noreply.github.com>
2025-09-03 07:14:29 +0000

    Rotate Array

---
+++ Array/RotateArray.java
@@ -0,0 +1,19 @@
+ //arr = 6 8 12 4 9 0  R(rotation)=2
+ //rotate karna hai
+ //d diya hota jiska matalb eg
+ //d=2 starting ke 2 elements ko rkh ke last mein rakh do
+ //array in place
+ //reverse krdo 2 parts ko//thrn poore ko ek sath reverse krdo
+ //or vice versa
+// 0 to d-1, d to n-1     // 0 = to n-1
+// d is number of rotations
+
+  
+
+
+
+package Array;
+
+public class RotateArray {
+    
+}
