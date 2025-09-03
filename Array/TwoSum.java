7589ed24cb7d1f59e2a9df8727359aae67b107d5
Nishant Sharma <165388083+Nishantsharma77@users.noreply.github.com>
2025-09-03 07:13:35 +0000

    two sum

---
+++ Array/TwoSum.java
@@ -0,0 +1,21 @@
+package Array;
+//arr={1,3,4,5,8}
+//target = 2
+//2 elements btane jinka sum target ke  raber bane
+
+//for(int i=0;i<n;i++){
+//for(int j=i+1;j<n;j++){
+    //compare
+    //if(arr[i]+ arr[j] ==target) 
+
+//}
+//solved on gfg platform
+public class TwoSum {
+    
+}
+
+//dry run code
+// {1,4,5,6,7,8}
+//  i,j       target=16//dono ko plus krte rho aur aage bdhte rho(j ko bdhate rehna)//
+//for every element loop chlega
+//ek ko fix krna ek move
